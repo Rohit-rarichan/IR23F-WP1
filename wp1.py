e53c6084b1cb3ec1a98f4c29f8121c2fa71e3271
Rohit George Rarichan <rraricha@uci.edu>
2024-01-29 23:52:12 -0800

    Final save and read me file

--- wp1.py
+++ wp1.py
@@ -3,14 +3,13 @@ from pathlib import Path
 def reverse_file(input_file_path, output_file_path):
     with open(Path(input_file_path), 'r') as input_file:
         file_size = input_file.seek(0, 2)  
-        reversed_content = ""
+        reversed_content = ""    #empty string to add reversed contents
         for pointerposition in (file_size-1, 0, -1):
             input_file.seek(pointerposition)
             char = input_file.read(1)
-            reversed_content += char
-        print(reversed_content)
+            reversed_content += char  #adding the reversed characters
     with open(Path(output_file_path), 'a') as output_file:
-        output_file.write(reversed_content)
+        output_file.write(reversed_content)   #writing the reversed characters onto the new file 
 
 
 # Example usage
