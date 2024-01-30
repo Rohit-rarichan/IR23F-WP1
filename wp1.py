5d0d5c6343b6817cdbd81c7e6210efe9838ba186
Rohit George Rarichan <rraricha@uci.edu>
2024-01-29 23:44:07 -0800

    Adding the code to write into the new file

--- wp1.py
+++ wp1.py
@@ -4,13 +4,13 @@ def reverse_file(input_file_path, output_file_path):
     with open(Path(input_file_path), 'r') as input_file:
         file_size = input_file.seek(0, 2)  
         reversed_content = ""
-        for pointerposition in (file_size-1, -1, -1):
+        for pointerposition in (file_size-1, 0, -1):
             input_file.seek(pointerposition)
             char = input_file.read(1)
             reversed_content += char
         print(reversed_content)
-    
-
+    with open(Path(output_file_path), 'a') as output_file:
+        output_file.write(reversed_content)
 
 
 # Example usage
