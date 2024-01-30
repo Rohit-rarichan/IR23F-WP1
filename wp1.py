fd8a9740c7871dfc9a96e9348efedcc2095a3488
Rohit George Rarichan <rraricha@uci.edu>
2024-01-29 23:37:17 -0800

    Adding the code ot reverse the string input

--- wp1.py
+++ wp1.py
@@ -1,14 +1,20 @@
+from pathlib import Path
+
 def reverse_file(input_file_path, output_file_path):
-    with open(input_file_path, 'r') as input_file:
+    with open(Path(input_file_path), 'r') as input_file:
         file_size = input_file.seek(0, 2)  
         reversed_content = ""
         for pointerposition in (file_size-1, -1, -1):
-            
+            input_file.seek(pointerposition)
+            char = input_file.read(1)
+            reversed_content += char
+        print(reversed_content)
+    
 
 
 
 # Example usage
 input_file_path = input("This is the file with the content in it ")
 output_file_path = input("This is the file you want to write in")
-
+reverse_file(input_file_path, output_file_path)
 
